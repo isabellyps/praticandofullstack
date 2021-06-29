408f0420566f7e8f847d5a4eaa6a80171412a8c8
Isabelly <isabelly.paganini@gmail.com>
2021-06-29 15:34:19 -0300

    Estrutura do módulo de autenticação

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './navegacao/home/home.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  {
+    path: 'conta',
+    loadChildren: () =>
+      import('./conta/conta.module').then((m) => m.ContaModule),
+  },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
