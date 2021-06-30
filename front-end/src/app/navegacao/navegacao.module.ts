ea5c062ed278617f6bcbef61f6b2f69e22f24d4d
Isabelly <isabelly.paganini@gmail.com>
2021-06-30 11:20:03 -0300

    Tratando um usuário logado

--- front-end/src/app/navegacao/navegacao.module.ts
+++ front-end/src/app/navegacao/navegacao.module.ts
@@ -5,12 +5,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { MenuLoginComponent } from './menu-login/menu-login.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
     declarations: [
         MenuComponent,
+        MenuLoginComponent,
         HomeComponent,
         FooterComponent,
         NotFoundComponent
